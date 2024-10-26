456baa58445162d65bcc98578c945a7c7bbafbd3
Olenka <pulya660@gmail.com>
2024-10-26 08:51:45 +0300

    HW16_AtataTextBoxTest

---
+++ AtataDemoQA/AtataDemoQA/TextBoxPage.cs
@@ -0,0 +1,38 @@
+﻿using Atata;
+using _ = AtataDemoQA.TextBoxPage;
+
+namespace AtataDemoQA
+{
+    [Url("/text-box")]
+    public sealed class TextBoxPage : Page<_>
+    {
+        [FindByPlaceholder("Full Name")]
+        public TextInput<_> FullName { get; set; }
+
+        [FindById("name")]
+        public Text<_> FullNameOutput { get; set; }
+
+        [FindByPlaceholder("name@example.com")]
+        public EmailInput<_> Email { get; set; }
+
+        [FindById("email")]
+        public Text<_> EmailOutput { get; set; }
+
+        [FindByPlaceholder("Current Address")]
+        public TextArea<_> CurrentAddress { get; set; }
+
+        [FindByCss("#output #currentAddress")]
+        public Text<_> CurrentAddressOutput { get; set; }
+
+        [FindById("permanentAddress")]
+        public TextArea<_> PermanentAddress { get; set; }
+        
+        [FindByCss("#output #permanentAddress")]
+        public Text<_> PermanentAddressOutput { get; set; }
+
+        [FindById("submit")]
+        public Button<_> Submit { get; set; }
+
+
+    }
+}
---
+++ AtataDemoQA/AtataDemoQA/TextBoxTests.cs
@@ -0,0 +1,26 @@
+﻿using Atata;
+using NUnit.Framework;
+using System;
+
+namespace AtataDemoQA
+{
+    public sealed class TextBoxTests : UITestFixture
+    {
+        [Test]
+        public void FillInputsAndVerifyOutputsOfTextBox()
+        {
+            Go.To<TextBoxPage>().
+                FullName.Set("Dog Pulya").
+                Email.Set("pulyadog@ask.com").
+                CurrentAddress.Set("Varash city, Budivelnikiv street, building 3, apartment 333").
+                PermanentAddress.Set("03549,Kyiv city, Zhylyanska street, building 1, apartment 111").
+            ScrollDown().
+            Submit.Click().
+                FullNameOutput.Should.Be("Name:Dog Pulya").
+                EmailOutput.Should.Be("Email:pulyadog@ask.com").
+                CurrentAddressOutput.Should.Be("Current Address :Varash city, Budivelnikiv street, building 3, apartment 333").
+                PermanentAddressOutput.Should.Be("Permananet Address :03549,Kyiv city, Zhylyanska street, building 1, apartment 111");
+
+        }
+    }
+}
